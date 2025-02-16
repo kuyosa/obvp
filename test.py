a8d346b2f04593a1bd9a6303735a89aeab76bbc2
kuyosa <uywrrt@mail.ru>
2025-02-16 21:39:10 +0300

    complete tests

--- test.py
+++ test.py
@@ -14,6 +14,13 @@ def test_read_main():
 #     data = response.json()
 #     assert len(data) > 0
 #     assert data[0]["username"] == "string"
+user_id = ''
+def set_user_id(id):
+    global user_id
+    user_id = id
+def get_user_id():
+    global user_id
+    return user_id
 
 
 def test_create_user():
@@ -27,9 +34,11 @@ def test_create_user():
     )
     assert response2.status_code == 400
     assert response.status_code == 200
+    
     data = response.json()
     assert data["username"] == "testuser"
     assert data["email"] == "testuser@example.com" 
+    set_user_id(data['id'])
 
 def test_auth_user():
     response = client.post(
@@ -81,7 +90,7 @@ def test_change_user():
     )
     data = response.json()
     response = client.put(
-        f'/users/1',
+        f'/users/{get_user_id()}',
         json={'username': 'testuser1', "email": "testuser123@mail.ru"},
         headers={'Authorization': f'Bearer {data['access_token']}'}
     )
@@ -89,6 +98,12 @@ def test_change_user():
     data = response.json()
     assert data['username'] == 'testuser1'
     assert data['email'] == 'testuser123@mail.ru'
+    response = client.put(
+        f'/users/{get_user_id()}',
+        json={'username': 'testuser1', "email": "testuser123@mail.ru"},
+        headers={'Authorization': f'Bearer 123'}
+    )
+    assert response.status_code == 401
 
 def test_delete_user():
     response = client.post(
@@ -97,8 +112,13 @@ def test_delete_user():
     )
     data = response.json()
     response = client.delete(
-        'users/1',
+        f'users/{get_user_id()}',
         headers={'Authorization': f'Bearer {data['access_token']}'}
     )
     assert response.status_code == 200
+    response = client.delete(
+        f'users/{get_user_id()}',
+        headers={'Authorization': f'Bearer {data['access_token']}'}
+    )
+    assert response.status_code == 401
     
